cb649dda16e5747c8736b1642dc7c5df8eb4eefd
lamtd <lamtd@vng.com.vn>
2023-01-10 07:53:10 +0700

    update config & add some quantized functions

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 # Import các thư viện cần thiết & một số hằng số
 
-from keras.utils.data_utils import get_file
 import tensorflow as tf
 from tensorflow.keras.applications.efficientnet import EfficientNetB2
-from tensorflow.keras.layers import Dense, GlobalAveragePooling2D, BatchNormalization
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D, BatchNormalization,Input
 from tensorflow.keras.metrics import CategoricalAccuracy
 from tensorflow.keras.models import Model
 from tensorflow.keras.optimizers import SGD
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from datetime import datetime
+
+dateStr = datetime.now().strftime("%Y%m%d-%H:%M:%S")
 
 INPUT_SIZE = (224, 224)
 NUM_CLASSES = 16
-PATCH_SIZE = 64
+BATCH_SIZE = 1
 MODEL_NAME = 'EfficientNetB2'
 DATASET_FOLDER = '.'
 LEARNING_RATE = 0.01
@@ -19,23 +21,28 @@ EPOCHS = 50
 MOMENTUM = 0.1
 MODEL_LINK = 'https://drive.google.com/file/d/1-omJp4YaAL4cczyOIiPzPVVtV2Qxs-UF/view?usp=share_link'
 
-base_model = EfficientNetB2(include_top=False, weights='imagenet')
+IMG_SHAPE = (224, 224, 3)
+inputs = Input(shape=IMG_SHAPE, batch_size=BATCH_SIZE)
+base_model = EfficientNetB2(include_top=False, weights='imagenet', input_tensor=inputs)
 
 # Load dữ liệu
 gen = ImageDataGenerator()
 
 test = gen.flow_from_directory(DATASET_FOLDER + '/test/',
                                target_size=INPUT_SIZE,
-                               batch_size=PATCH_SIZE,
+                               batch_size=BATCH_SIZE,
                                shuffle=False)
+
 
 def printDevices():
     print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
-    resolver = tf.distribute.cluster_resolver.TPUClusterResolver(tpu='')
+    # detect the TPU
+    resolver = tf.distribute.cluster_resolver.TPUClusterResolver()
     tf.config.experimental_connect_to_cluster(resolver)
     # This is the TPU initialization code that has to be at the beginning.
     tf.tpu.experimental.initialize_tpu_system(resolver)
     print("All devices: ", tf.config.list_logical_devices('TPU'))
+
 
 # Xây dựng model dựa trên các backbone thông dụng (VGG,Resnet,Xception,Inception...)
 def getModel():
@@ -78,13 +85,37 @@ def getCustomModel():
 sgd = SGD(learning_rate=LEARNING_RATE, momentum=MOMENTUM)
 acc = CategoricalAccuracy(name='acc')
 
+from tensorflow import lite
+import numpy as np
+
+
+def representative_dataset():
+    for _ in range(100):
+      data = np.random.rand(1, 244, 244, 3)
+      yield [data.astype(np.float32)]
+
+
+def convertToLite(modelFile):
+    converter = lite.TFLiteConverter.from_keras_model(modelFile)
+    converter.optimizations = [tf.lite.Optimize.DEFAULT]
+    converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
+    converter.representative_dataset = representative_dataset
+    converter.inference_input_type = tf.uint8
+    converter.inference_output_type = tf.uint8
+    tfmodel = converter.convert()
+    open(f"model_{MODEL_NAME}_{dateStr}.tflite", "wb").write(tfmodel)
+
 
 def singleTest():
     model2 = getModel()
     model2.compile(optimizer=sgd, loss='categorical_crossentropy', metrics=[acc])
-    model2.load_weights('model.hdf5')
-    print(f'test:', model2.evaluate(test))
+    model2.load_weights(f'model_{MODEL_NAME}.hdf5')
+    model2.summary()
+    # print(f'test:', model2.evaluate(test))
+    return model2
+
 
 if __name__ == '__main__':
-    printDevices()
+    # printDevices()
+    # convertToLite(singleTest())
     singleTest()
