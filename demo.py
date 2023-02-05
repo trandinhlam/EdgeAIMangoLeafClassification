8d164967c289a2610b6cc9441ef76cf6662904c0
lamtd <lamtd@vng.com.vn>
2023-02-05 18:28:59 +0700

    add demo file

--- demo.py
+++ demo.py
@@ -6,23 +6,28 @@ import edge_inference
 # const
 video_file = './demo/How To Control Caterpillars On Plants Easy (IN HINDI) Caterpillars Eating Leaves.mp4'
 model_file = './model_edge_tpu/model_full_integer_ResNet50_20230205-10:52:35_edgetpu.tflite'
+rate_ratio = 2
 # run
 if __name__ == '__main__':
     # load video file
     video = cv2.VideoCapture(video_file)
-    fps = video.get(cv2.CAP_PROP_FPS)
+    fps = video.get(cv2.CAP_PROP_FPS)/rate_ratio
 
     interpreter = edge_inference.load_interpreter(model_file)
 
+    counter = 0
     while True:
         # cut frames & show frame
         ret, frame = video.read()
+        counter += 1
+        inference_time = 0
         cv2.imshow("Video", frame)
-        start = time.perf_counter()
-        # todo: pest classification
-        predict = lite_inference.do_inference(interpreter, frame)
-        inference_time = time.perf_counter() - start
-        print(predict, inference_time)
+        if counter % rate_ratio == 0:
+            start = time.perf_counter()
+            # todo: pest classification
+            predict = lite_inference.do_inference(interpreter, frame)
+            inference_time = time.perf_counter() - start
+            print(predict, inference_time)
         # todo: show titles & position & temperature & light info on screen
         # cv2.imshow("Heatmap", image_heat)
         gap = min(abs(fps / 1000 - inference_time), 0)
