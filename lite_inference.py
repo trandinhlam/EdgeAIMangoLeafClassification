5f35db9503f4113ea46b7bbe1cff49a403e2d237
tdlam <tranthienhoang14@gmail.com>
2023-12-16 19:03:14 +0700

    create server with response

--- lite_inference.py
+++ lite_inference.py
@@ -39,22 +39,50 @@ def do_inference(interpreter, test_img):
     print(predict, label)
     return class_id, list(label)[0]
 
+from flask import Flask, jsonify, request
+import base64
+from PIL import Image
+from io import BytesIO
+import os
+from flask_cors import CORS  # Import the CORS extension
+
+app = Flask(__name__)
+CORS(app)
+
+interpreter = load_interpreter('model_lite/model_full_integer_ResNet18_20230723-10_57_21.tflite')
+folder_path = 'img_inference' 
+
+# Get a list of all files in the folder
+files = os.listdir(folder_path)
+image_files = [file for file in files if file.lower().endswith(('.jpeg', '.jpg'))]
+
+@app.route('/api/data', methods=['GET'])
+def load_data():
+    resp = []
+    for image_file in image_files:
+        image_path = os.path.join(folder_path, image_file)
+        img = Image.open(image_path)
+        predict, label = do_inference(interpreter, np.uint8(img))
+        print(predict, label, image_file)
+        # Convert image data to base64
+        buffered = BytesIO()
+        img = img.resize((500, 500))
+        img.save(buffered, format="PNG")  # Change the format as needed
+        img_base64 = base64.b64encode(buffered.getvalue()).decode("utf-8") 
+        resp.append({"label": label, "img": img_base64})
+    return jsonify(resp)
+
+
+
 if __name__ == '__main__':
+    app.run(debug=True)
     # interpreter = load_model('model_lite/model_full_integer_ResNet50_20230205-10:12:18.tflite')
     # interpreter = load_interpreter('model_lite/model_full_integer_ResNet8_20230219-08:20:54.tflite')
-    interpreter = load_interpreter('model_lite/model_full_integer_ResNet18_20230723-10_57_21.tflite')
     # interpreter = load_model(get_model_path())
     # test_gen = utils.get_test_data_generator()
     # acc = utils.calculate_acc(interpreter, test_gen)
     # print(acc)
-    
-    folder_path = 'img_inference'  # Change this to the path of your folder
-
-    # Get a list of all files in the folder
-    files = os.listdir(folder_path)
-
     # Filter the list to include only files with certain extensions (e.g., '.jpeg' or '.jpg')
-    image_files = [file for file in files if file.lower().endswith(('.jpeg', '.jpg'))]
 
     # Iterate through each image file and open it
     for image_file in image_files:
